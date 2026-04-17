eefb5298fc188e1329624f25be80e6d7d1679496
AurelioBastos <a.pereirabastos@gmail.com>
2026-04-17 15:35:19 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1002,4 +1002,4 @@ async def export_confronto(payload: ExportConfrontoPayload):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
